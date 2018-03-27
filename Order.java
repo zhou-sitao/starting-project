f2925f540c43c59a97d48a69260c7954376de3f4
zst <zhou.sitao@mhccenter.com>
2018-03-27 14:37:57 +0800

    commit merge

--- Order.java
+++ Order.java
@@ -2,14 +2,7 @@ public class Order {
 	
 	private int id;
 	private String name;
-	private int litsId;
-	
-	public int getId(){
-		return id;
-	}
-	public void setId(int id){
-		this.id = id;
-	}
+	private int orderListId;
 	
 	public int getId(){
 		return id;
