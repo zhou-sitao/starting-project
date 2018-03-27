abedc015277e9b031040c3b7b8dcc5be272a1117
zst <zhou.sitao@mhccenter.com>
2018-03-27 11:50:39 +0800

    add and modify Order.java

--- Order.java
+++ Order.java
@@ -2,6 +2,13 @@ public class Order {
 	
 	private int id;
 	private String name;
-	private int orderListId;
+	private int listId;
+	
+	public int getId(){
+		return id;
+	}
+	public void setId(int id){
+		this.id = id;
+	}
 	
 }
